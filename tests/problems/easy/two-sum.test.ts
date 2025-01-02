df49230c066e5987cd8a24783498f5182d3ef2d3
牧知 <muzhi.xuan@tuya.com>
2025-01-02 16:46:08 +0800

    中文用例描述

--- tests/problems/easy/two-sum.test.ts
+++ tests/problems/easy/two-sum.test.ts
@@ -2,13 +2,13 @@ import { test, expect } from "@playwright/test";
 import { twoSum } from "../../../src/problems/easy/two-sum";
 
 test.describe("Two Sum", () => {
-  test("should return indices of two numbers that add up to target", () => {
+  test("应该返回两个数的索引，它们的和等于目标值", () => {
     expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
     expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
     expect(twoSum([3, 3], 6)).toEqual([0, 1]);
   });
 
-  test("should return empty array when no solution exists", () => {
+  test("当没有解决方案时应该返回空数组", () => {
     expect(twoSum([1, 2, 3], 7)).toEqual([]);
   });
 });
